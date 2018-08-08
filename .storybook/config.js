95a099d8503c25fd9bbe2188d95421d93ae8f7ec
masahitojp <randomstep@gmail.com>
2018-08-09 02:41:56 +0900

    refactor: :+1: change storybook settings https://qiita.com/mizchi/items/5f1a23f1507a026f0509

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { configure } from '@storybook/react'
-const req = require.context('../src/components', true, /.stories.(ts|tsx)$/)
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  const req = require.context('../src/components', true, /.stories.(ts|tsx)$/)
+  req.keys().forEach(req)
 }
 
 configure(loadStories, module)
